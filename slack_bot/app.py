cf31d43d8ec95284047ee5daeb93a994ff2df2b3
knhn1004 <49494541+knhn1004@users.noreply.github.com>
2024-06-22 15:55:41 -0700

    feat: Add integration with Bedrock Agent for natural language processing
    This commit adds integration with the Bedrock Agent for natural language processing in the Slack bot lambda function. It includes the following changes:
    
    - Importing the necessary dependencies (boto3, uuid, botocore)
    - Configuring the Bedrock Agent runtime client with appropriate timeouts and retries
    - Invoking the Bedrock Agent with user input text and retrieving the response
    - Posting the response from the Bedrock Agent to the Slack channel
    
    The requirements.txt file is also updated to include the boto3 dependency.

--- slack_bot/app.py
+++ slack_bot/app.py
@@ -1,9 +1,21 @@
 import json
 import os
+import boto3
+import uuid
+import botocore
+
 from slack_sdk import WebClient
 
 BOT_TOKEN = os.environ.get('SLACK_BOT_TOKEN')
 client = WebClient(token=BOT_TOKEN)
+BEDROCK_ID = os.environ.get('BEDROCK_ID')
+BEDROCK_ALIAS = os.environ.get('BEDROCK_ALIAS')
+bedrock_client = boto3.client('bedrock-agent-runtime')
+br_config = botocore.config.Config(read_timeout=900,
+                                   connect_timeout=900,
+                                   retries={"max_attempts": 0})
+bedrock_agent_runtime = boto3.client(
+    "bedrock-agent-runtime", region_name="us-west-2", config=br_config)
 
 
 def lambda_handler(event, context):
@@ -18,15 +30,36 @@ def lambda_handler(event, context):
 
         if event_type == "app_mention":
             channel_id = data.get("channel")
+            text = data.get("text")
+            user = data.get("user")
+
+            session_id = str(uuid.uuid4())
+            response = bedrock_agent_runtime.invoke_agent(
+                agentId=BEDROCK_ID,      # Identifier for Agent
+                agentAliasId=BEDROCK_ALIAS.split(
+                    '|')[1],  # Identifier for Agent Alias
+                sessionId=session_id,    # Identifier used for the current session
+                inputText=f"{user}: {text}",  # Input
+            )
+
+            output = ""
+            stream = response.get('completion')
+            if stream:
+                for event in stream:
+                    chunk = event.get('chunk')
+                    if chunk:
+                        output += chunk.get('bytes').decode()
+
             client.chat_postMessage(
-                channel=channel_id, text="👋 Hello there! I'm Serverless Slack App, here to make your day a little easier. 😊")
+                channel=channel_id, text=output)
+
         return
     else:
         return {
             "statusCode": 200,
             "body": json.dumps(
                 {
-                    "message": "hello world",
+                    "message": "ok",
                 }
             ),
         }
